e7fd80ffacfcc0dc52cee5571b509bde275e0f0f
BaldracchiJonas <baldracchijonas@gmail.com>
2023-07-29 23:23:12 -0300

    create tables orders,orders_lines,products

---
+++ database/migrations/2023_07_30_020141_create_orders_lines_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersLinesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('orders_lines', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('order_id');
+            $table->integer('qty');
+            $table->unsignedBigInteger('product_id');
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('orders_lines');
+    }
+}
